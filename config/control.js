ff786d9525ffe2e3927072ab3acb09c9175d3a26
gmf <16450382@qq.com>
2018-03-21 14:47:31 +0800

    add new controls to control.js

--- config/control.js
+++ config/control.js
@@ -1,9 +1,9 @@
 import { Login as Login1 } from 'yes-native';
 import { Login as Login2 } from 'yes-native';
 import { CellLayoutList as CellLayoutList } from 'yes-native';
-import  DateText from '../template/ListTemplate/DateText';
+import DateText from '../template/ListTemplate/DateText';
+import { Timeline } from 'yes-native';
+import Avatar from '../controls/Avatar';
+import AuditIcon from '../controls/AuditStatusIcon';
 
-
-
-console.log(Login1, Login2)
-export default { Login1, Login2, CellLayoutList, DateText };
+export default { Login1, Login2, CellLayoutList, DateText, Timeline, Avatar, AuditIcon };
